0095a03f28f1c94bb5d97f5dc4e03d5df57016ad
Hamid Rahman <waterbrain342@gmail.com>
2025-07-12 11:34:07 +0500

    Social Auth via google repushed

--- account/views.py
+++ account/views.py
@@ -37,6 +37,8 @@ def register(request):
             new_user.save()
             Profile.objects.create(user=new_user)
             return render(request,'account/register_done.html',{'new_user': new_user})
+        else:
+            return render(request, 'account/register.html', {'user_form': user_form})
     else:
         user_form = UserRegistrationForm()
         return render(request, 'account/register.html',{'user_form': user_form})
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,28 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-2rvz-kc2b9o3(c1tlh97lapliii=k*)vmu5b=(hbeili9+cd3i'
 
+#Social Auth Keys
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_OAUTH2_SECRET')
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+
+SOCIAL_AUTH_PIPELINE = [
+'social_core.pipeline.social_auth.social_details',
+'social_core.pipeline.social_auth.social_uid',
+'social_core.pipeline.social_auth.auth_allowed',
+'social_core.pipeline.social_auth.social_user',
+'social_core.pipeline.user.get_username',
+'social_core.pipeline.user.create_user',
+'account.authentication.create_profile',
+'social_core.pipeline.social_auth.associate_user',
+'social_core.pipeline.social_auth.load_extra_data',
+'social_core.pipeline.user.user_details',
+]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mysite.com']
 
 
 # Application definition
@@ -43,8 +62,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    #3rd Party Apps
+    'social_django', # This may be the defaul application ???
+    'django_extensions',
     #My Apps
-
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -109,6 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
 ]
 
 LOGIN_REDIRECT_URL = 'dashboard'
